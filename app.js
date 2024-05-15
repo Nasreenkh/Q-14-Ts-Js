5a3b43912f758e071ead79a01d7e9a44bf233d8c
Nasreen khan <160172100+Nasreenkh@users.noreply.github.com>
2024-05-16 00:41:22 +0500

    This is 45 Question Assignment Q-14.
    This is 45 Question Assignment Q-14.

---
+++ app.js
@@ -0,0 +1,4 @@
+var guest_list = ['Nida', 'Anum', 'Mehak', 'Yasir', 'Vaniza'];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam ' + guest_list[i] + '\nWe invite you to dinner tomorrow. Thank you.');
+}
---
+++ app.ts
@@ -0,0 +1,14 @@
+//question no 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make
+//a list that includes at least three people you’d like to invite to dinner. Then use your list to print
+//a message to each person, inviting them to dinner. 
+
+
+
+
+
+
+let guest_list: string[] = ['Nida', 'Anum', 'Mehak', 'Yasir', 'Vaniza'];
+
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam ' + guest_list[i] + '\nWe invite you to dinner tomorrow. Thank you.');
+}
